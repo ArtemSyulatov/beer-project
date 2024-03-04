30407f177bc860b8099f81ddbe3dd472302c00dd
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2024-03-05 01:04:13 +0500

    fix: protected routes

--- src/router/ProtectedRouteAuth.tsx
+++ src/router/ProtectedRouteAuth.tsx
@@ -1,13 +1,14 @@
 import { Navigate } from 'react-router-dom';
 import React from 'react';
 import { useAppSelector } from '../hooks/redux';
+import { getIsAuth } from '../redux-toolkit/selectors/getIsAuth';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const ProtectedRouteAuth = ({ children }: Props) => {
-  const isAuth = useAppSelector((state) => state.auth.auth);
+  const isAuth = useAppSelector(getIsAuth);
   if (isAuth) {
     return <Navigate to="/" replace />;
   }
