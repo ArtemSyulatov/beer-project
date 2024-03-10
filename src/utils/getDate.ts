90ea9fc0429264fbc70a656d6e7db117422fc221
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2024-03-10 13:01:26 +0500

    fix: fix getDate func

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -2,4 +2,4 @@ import { SearchHistory } from '../types/SearchHistory';
 
 export const getDate = (): string => new Date().toJSON();
 export const getDateForUi = (historyItem: SearchHistory): string =>
-  new Date(historyItem.date).toLocaleString().slice(0, 8);
+  new Date(historyItem.date).toLocaleString().slice(0, 9);
