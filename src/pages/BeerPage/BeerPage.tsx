ebec96c40aa68e3633abfe9c10d36edee52e88eb
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2024-03-10 13:17:40 +0500

    fix: add favourites btn for beer page

--- src/pages/BeerPage/BeerPage.tsx
+++ src/pages/BeerPage/BeerPage.tsx
@@ -2,6 +2,10 @@ import { useParams } from 'react-router-dom';
 import { beerApi } from '../../redux-toolkit/services/BeerService';
 import s from './BeerPage.module.scss';
 import { Loader } from '../../components/ui/Loader/Loader';
+import { Button } from '../../components/ui/Button/Button';
+import { useAppSelector } from '../../hooks/redux';
+import { getIsAuth } from '../../redux-toolkit/selectors/getIsAuth';
+import useFavorite from '../../hooks/useFavourite';
 
 const BeerPage = () => {
   const params = useParams();
@@ -10,6 +14,8 @@ const BeerPage = () => {
     isLoading,
     error,
   } = beerApi.useGetSingleBeerQuery(params.id);
+  const isAuth = useAppSelector(getIsAuth);
+  const { toggleFavorite, getIsFavourite } = useFavorite();
   if (isLoading) {
     return <Loader />;
   }
@@ -21,6 +27,17 @@ const BeerPage = () => {
       <h2>{beer.name}</h2>
       <img src={beer.imageUrl} alt="" />
       <p>{beer.description}</p>
+      {isAuth && (
+        <div className={s.favouriteBtn}>
+          <Button onClick={() => toggleFavorite(beer)}>
+            {getIsFavourite(beer) ? (
+              <p>Remove from favourites</p>
+            ) : (
+              <p>Add To Favourites</p>
+            )}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
