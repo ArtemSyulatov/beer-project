d3211dd29dcd0a85f2f7665b5ffa9bfb47551347
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2024-03-10 11:24:28 +0500

    feat: add type for middleware

--- src/redux-toolkit/middleware/isAuthMiddleware.ts
+++ src/redux-toolkit/middleware/isAuthMiddleware.ts
@@ -1,17 +1,22 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
 import { clearUser, setUser } from '../reducers/userSlice';
+import { AppDispatch, RootState } from '../store';
 
 export const listenerMiddleware = createListenerMiddleware();
 
-listenerMiddleware.startListening({
+export const startAppListening = listenerMiddleware.startListening.withTypes<
+  RootState,
+  AppDispatch
+>();
+
+startAppListening({
   actionCreator: clearUser,
   effect: () => {
     toast.success(`Logging Out!`);
   },
 });
-
-listenerMiddleware.startListening({
+startAppListening({
   actionCreator: setUser,
   effect: () => {
     toast.success('Login successful!');
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,5 +1,5 @@
 import { SearchHistory } from '../types/SearchHistory';
 
-export const getDate = () => new Date().toJSON();
+export const getDate = (): string => new Date().toJSON();
 export const getDateForUi = (historyItem: SearchHistory): string =>
   new Date(historyItem.date).toLocaleString().slice(0, 8);
